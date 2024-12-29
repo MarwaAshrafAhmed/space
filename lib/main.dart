f95af293ca4bcd75cd643cb943bfd511192b299c
Marwa_Ashraf <elshafeemarwa@gmail.com>
2024-12-29 11:30:53 -0800

    Change initial route to page one screen

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Earth(),
+      home: PageOne(),
       routes: {
         '/home':(_)=>HomeScreen(),
         '/page_one':(_)=>PageOne(),
