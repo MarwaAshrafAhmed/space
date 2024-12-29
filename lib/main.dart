37239b0c63111b5222d15a2a42b2c1f8d64a1dbc
Marwa_Ashraf <elshafeemarwa@gmail.com>
2024-12-29 11:28:06 -0800

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: Earth(),
       routes: {
         '/home':(_)=>HomeScreen(),
         '/page_one':(_)=>PageOne(),
